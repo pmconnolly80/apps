379fdb22c75960f571335ac7b1374e1b8a015598
Vincent C <vincedo@gmail.com>
2014-10-17 14:53:16 +0200

    changes

--- gmail/src/layouts/app.js
+++ gmail/src/layouts/app.js
@@ -1,25 +1,9 @@
 (function () {
   'use strict';
 
-  angular.module('appGmail', [])
+  angular.module('gmailApp', [])
 
-  .constant('FBURL', 'https://amber-fire-8058.firebaseio.com/')
-
-  .controller('MainCtrl', function($scope) {
-    var vm = this;
-
-    vm.doSomething = doSomething;
-
-    vm.total = [
-      {name: 'zzf'},
-      {name: 'zzf'},
-      {name: 'zzf'},
-      {name: 'zzf'}
-    ];
-
-    function doSomething() {
-
-    }
+  .controller('AppCtrl', function() {
   });
 
 })();
